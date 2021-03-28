b9baf2fa9ea444cadd17b94d72d7d0d0455b817a
jol1992 <mr.chiakajaylou@gmail.com>
2021-03-28 14:32:06 -0500

    added sec,minutes, and hours

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,17 +1,48 @@
+import { useEffect, useState } from 'react'
 import MainContainer from '../Body/MainContainer/MainContainer'
 import MenuItems from '../MenuItems/MenuItem'
 import './Hero.css'
 
 const Hero =() =>{
 
-    const daysLeft = Math.trunc((new Date("5/27/2021").getTime() - new Date())/(1000 *3600 *24))
+    // const daysLeft = Math.trunc((new Date("5/27/2021").getTime() - new Date())/(1000 *3600 *24))
+    const [days,setDays] =useState(0);
+    const [hours,setHours] =useState(0);
+    const [minutes,setMinutes] =useState(0);
+    const [seconds,setSeconds] =useState(0);
+
+    useEffect(
+        ()=>{
+            startTimer();
+        },[]
+    )
+
+
+    const startTimer =()=>{
+        setInterval(countDown,1000)
+    }
+
+    const countDown =()=>{
+        const dayOfTrip=new Date("5/26/2021").getTime();
+        const today =new Date().getTime();
+        const difference =  dayOfTrip - today
+
+        
+        
+        setSeconds(Math.floor((difference / 1000) % 60));
+        setMinutes(Math.floor((difference / 1000 / 60) % 60));
+        setHours(Math.floor((difference / (1000 * 60 * 60)) % 24));
+        setDays(Math.floor(difference / (1000 * 60 * 60 * 24)))
+        
+
+    }
 
     return(
         <>
         <div className={'Hero'}>
             <div>
             <h1><p>Disney</p> Vacation 2021</h1>
-            <p>{daysLeft} days left</p>
+            <p><span>{days}</span> days <span>{hours}</span> hrs <span>{minutes}</span> mins  <span>{seconds}</span>sec left</p>
             </div>
             
         </div>
